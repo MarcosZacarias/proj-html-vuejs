56aae12b313a22bff7fc31a7effcc85d365aefc8
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-09-19 15:19:35 +0200

    add setup files

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from "vue";
+
+import * as bootstrap from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./style/general.scss";
+
+import App from "./App.vue";
+
+createApp(App).mount("#app");
