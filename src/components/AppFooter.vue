0df22f334b9ea3bbeb9de00394082aa747a7ebe8
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-09-20 17:35:02 +0200

    add footer main

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -37,7 +37,7 @@ export default {
             closed: "Kitchen Closed",
           },
           {
-            day: "Tuseday until Friday",
+            day: "Tuesday until Friday",
             hours: "9:00 - 22:00",
           },
           {
@@ -50,6 +50,10 @@ export default {
           },
         ],
       },
+      socials: {
+        title: "Follow Us",
+        socialIcons: ["twitter", "facebook-f", "instagram", "linkedin-in"],
+      },
     };
   },
 
@@ -61,55 +65,112 @@ export default {
 
 <template>
   <footer class="d-flex">
-    <div class="w-50">
-      <div class="footer-main">
-        <div class="d-flex gap-5">
+    <div class="footer-main">
+      <div class="footer-top">
+        <!-- Locals -->
+        <div>
+          <!-- Restaurants -->
+          <ul>
+            <!-- Title -->
+            <li>
+              <h5 class="footer-title">{{ locals.titleLocal }}</h5>
+            </li>
+            <!-- Address locals -->
+            <li v-for="local in locals.restaurants">
+              <ul class="my-3 footer-text">
+                <li>{{ local.address }}</li>
+                <li>{{ local.city }}</li>
+                <li>{{ local.telNumber }}</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <!-- Timetables and Socials -->
+        <div>
+          <!-- Timetables -->
+          <ul>
+            <!-- Timetables Title -->
+            <li>
+              <h5 class="footer-title">{{ workHours.title }}</h5>
+            </li>
+            <!-- Day Hours -->
+            <li v-for="day in workHours.weekHours">
+              <ul class="my-3 footer-text">
+                <li>{{ day.day }}</li>
+                <li v-if="day.closed">{{ day.closed }}</li>
+                <li>{{ day.hours }}</li>
+              </ul>
+            </li>
+          </ul>
+          <!-- Socials -->
           <div>
-            <ul>
-              <li>
-                <h5>{{ locals.titleLocal }}</h5>
-              </li>
-              <li v-for="local in locals.restaurants">
-                <ul class="my-3">
-                  <li>{{ local.address }}</li>
-                  <li>{{ local.city }}</li>
-                  <li>{{ local.telNumber }}</li>
-                </ul>
-              </li>
-            </ul>
+            <!-- Title social -->
+            <span class="footer-title"> {{ socials.title }}: </span>
+            <!-- Icons -->
+            <span v-for="icon in socials.socialIcons" class="px-1">
+              <a href="#">
+                <font-awesome-icon :icon="['fab', `${icon}`]" />
+              </a>
+            </span>
           </div>
-          <div>
-            <ul>
-              <li>
-                <h5>{{ workHours.title }}</h5>
-              </li>
-              <li v-for="day in workHours.weekHours">
-                <ul class="my-3">
-                  <li>{{ day.day }}</li>
-                  <li v-if="day.closed">{{ day.closed }}</li>
-                  <li>{{ day.hours }}</li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-          <div></div>
         </div>
         <div></div>
       </div>
+      <div class="footer-bottom">
+        <div>
+          <span>Created with &hearts; .by <span>Qode Interactive</span></span>
+        </div>
+        <div>
+          <img src="../../public/img/layout/footer-bottom-img.png" alt="" />
+        </div>
+      </div>
     </div>
-    <div class="w-50">
-      <img src="../../public/img/layout/footer-img-1.jpg" alt="" />
-    </div>
+
+    <div class="footer-img-bg"></div>
   </footer>
 </template>
 
 <style lang="scss" scoped>
 .footer-main {
   color: #ffffff;
+  width: 50%;
+
+  padding: 5rem 1rem 2rem 1rem;
 
   height: 100%;
   background-image: url("../../public/img/layout/cielostellato.PNG");
   background-repeat: no-repeat;
+  background-size: cover;
+
+  .footer-top {
+    display: flex;
+    justify-content: space-around;
+    padding-bottom: 1rem;
+    > div {
+      width: calc(90% / 3);
+    }
+    .footer-title {
+      color: #b48d3a;
+      font-size: medium;
+      font-weight: bold;
+      text-transform: uppercase;
+    }
+    .footer-text {
+      color: rgb(172, 172, 172);
+    }
+  }
+
+  .footer-bottom {
+    display: flex;
+    justify-content: space-between;
+    align-items: end;
+    padding: 0 1rem;
+  }
+}
+.footer-img-bg {
+  width: 50%;
+  background-image: url("../../public/img/layout/footer-img-1.jpg");
+  background-position: center;
   background-size: cover;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -7,4 +7,12 @@ import "./style/general.scss";
 
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
+library.add(fab);
+
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
